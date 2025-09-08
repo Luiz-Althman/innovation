045862b114f019671c5450bd3adbba2a53b9a031
luiz <luizalthmandev@gmail.com>
2025-09-08 16:03:07 -0300

    chore: add eslint, prettier and shadcn setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+  ...compat.extends('@rocketseat/eslint-config/next'),
+  ...compat.extends('next/core-web-vitals'),
+]
 
-export default eslintConfig;
+export default eslintConfig
